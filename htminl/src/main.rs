375894da71139feaa502a8f3e5ab1297c3695b1b
Josh Stoik <josh@blobfolio.com>
2020-03-27 17:07:32 -0700

    cleanup: simplify logic add: --list flag

--- htminl/src/main.rs
+++ htminl/src/main.rs
@@ -21,6 +21,7 @@ mod menu;
 
 use clap::ArgMatches;
 use fyi_core::{
+	Msg,
 	Progress,
 	progress_arc,
 	witcher,
@@ -31,7 +32,6 @@ use fyi_core::witcher::ops::FYIOps;
 use hyperbuild::hyperbuild;
 use rayon::prelude::*;
 use std::path::PathBuf;
-use std::sync::atomic::{AtomicU64, Ordering};
 use std::time::Instant;
 
 
@@ -41,54 +41,52 @@ fn main() -> Result<(), String> {
 	let opts: ArgMatches = menu::menu()
 		.get_matches();
 
-	// What path are we dealing with?
-	let mut paths: Vec<PathBuf> = opts.values_of("path").unwrap()
-		.into_iter()
-		.filter_map(|x| Some(PathBuf::from(x)))
-		.collect();
-
 	let pattern = witcher::pattern_to_regex(r"(?i).+\.html?$");
-	paths.fyi_walk_filtered_mut(&pattern);
+
+	// What path are we dealing with?
+	let paths: Vec<PathBuf> = match opts.is_present("list") {
+		false => {
+			let tmp: Vec<PathBuf> = opts.values_of("path").unwrap()
+				.into_iter()
+				.filter_map(|x| Some(PathBuf::from(x)))
+				.collect();
+
+			tmp.fyi_walk_filtered(&pattern)
+		},
+		true => PathBuf::from(opts.value_of("list").unwrap_or(""))
+			.fyi_walk_file_lines(Some(pattern)),
+	};
 
 	if paths.is_empty() {
 		return Err("No HTML files were found.".to_string());
 	}
 
-	let found: u64 = paths.len() as u64;
-	let time = Instant::now();
-	let progress: bool = opts.is_present("progress");
-	let summary: bool = opts.is_present("summary");
-	let bar = Progress::new("", paths.len() as u64, PROGRESS_NO_ELAPSED);
+	// With progress.
+	if opts.is_present("progress") {
+		let found: u64 = paths.len() as u64;
+		let bar = Progress::new("", found, PROGRESS_NO_ELAPSED);
+		let before: u64 = paths.fyi_file_sizes();
+		let time = Instant::now();
 
-	let old_len = AtomicU64::new(0);
-	let new_len = AtomicU64::new(0);
+		paths.clone().into_par_iter().for_each(|x| {
+			let _ = x.encode().is_ok();
 
-	paths.into_par_iter().for_each(|ref x| {
-		if let Ok((ol, nl)) = x.encode() {
-			old_len.fetch_add(ol, Ordering::SeqCst);
-			new_len.fetch_add(nl, Ordering::SeqCst);
+			progress_arc::set_path(bar.clone(), x.to_path_buf());
+			progress_arc::increment(bar.clone(), 1);
+			progress_arc::tick(bar.clone());
+		});
 
-			// Update progress?
-			if true == progress {
-				progress_arc::set_path(bar.clone(), &x);
-				progress_arc::increment(bar.clone(), 1);
-				progress_arc::tick(bar.clone());
-			}
-		}
-	});
-
-	// Finish progress bar if applicable.
-	if true == progress {
 		progress_arc::finish(bar.clone());
-	}
 
-	if true == summary {
-		witcher::walk_summary(
-			found,
-			time,
-			old_len.load(Ordering::SeqCst),
-			new_len.load(Ordering::SeqCst)
-		);
+		let after: u64 = paths.fyi_file_sizes();
+		Msg::msg_crunched_in(found, time, Some((before, after)))
+			.print();
+	}
+	// Without progress.
+	else {
+		paths.into_par_iter().for_each(|ref x| {
+			let _ = x.encode().is_ok();
+		});
 	}
 
 	Ok(())
@@ -97,25 +95,22 @@ fn main() -> Result<(), String> {
 /// Encoding!
 pub trait HTMinLEncode {
 	/// Encode.
-	fn encode(&self) -> Result<(u64, u64), String>;
+	fn encode(&self) -> Result<(), String>;
 }
 
 impl HTMinLEncode for PathBuf {
 	/// Encode.
-	fn encode(&self) -> Result<(u64, u64), String> {
+	fn encode(&self) -> Result<(), String> {
 		// Load it.
 		let mut data = self.fyi_read()?;
-		let old_len: u64 = data.len() as u64;
-		let mut new_len: u64 = old_len;
 
 		if let Ok(len) = hyperbuild(&mut data) {
 			// Save it?
 			if 0 < len {
 				self.fyi_write(&data[..len])?;
-				new_len = len as u64;
 			}
 
-			return Ok((old_len, new_len));
+			return Ok(());
 		}
 
 		Err("Unable to minify file.".into())

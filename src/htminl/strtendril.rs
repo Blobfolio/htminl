ad851e49688633e12cf46f608991f596aa50a28f
Josh Stoik <josh@blobfolio.com>
2021-12-05 22:52:27 -0800

    misc: remove unsafe

--- src/htminl/strtendril.rs
+++ src/htminl/strtendril.rs
@@ -23,9 +23,9 @@ use tendril::StrTendril;
 /// override the display state of any element arbitrarily, so we are *not*
 /// doing that here.
 pub(super) fn collapse_whitespace(txt: &mut StrTendril) {
-	let alter = StrTendril::from(unsafe {
+	let alter = StrTendril::from({
 		let mut in_ws: bool = false;
-		std::str::from_utf8_unchecked(&txt.as_bytes()
+		String::from_utf8_lossy(&txt.as_bytes()
 			.iter()
 			.filter_map(|c| match *c {
 				b'\t' | b'\n' | b'\x0C' | b'\r' | b' ' =>
@@ -40,6 +40,7 @@ pub(super) fn collapse_whitespace(txt: &mut StrTendril) {
 				},
 			})
 			.collect::<Vec<u8>>())
+			.as_ref()
 	});
 
 	if (*txt).ne(&alter) {
